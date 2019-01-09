8b46cdcbb05acdd64f170fc096e036cb83964acf
Tobias Norton <tnorton20@yahoo.com>
2019-01-08 19:06:02 -0500

    React app inside Rails application

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
     resources :questions
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  if Rails.env.production?
+    CLIENT_HTML = File.read(Rails.root.join('public/index.html'))
+
+    get "*path", to: proc { [200, {}, [CLIENT_HTML]] }
+  end
 end
